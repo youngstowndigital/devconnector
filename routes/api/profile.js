29c6785a92580dfca278e23825fc807e0258840a
Mike Wittenauer <mikewittenauer@me.com>
2020-09-23 20:21:53 -0400

    Delete profile/user route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -124,6 +124,22 @@ router.get('/user/:user_id', async (req, res) => {
         }
         res.status(500).send('Server Error');
     }
+});
+
+// @route    DELETE api/profile
+// @desc     Delete profile, user and posts
+// @access   Private
+router.delete('/', async (req, res) => {
+    try {
+        await Profile.findOneAndRemove({ user: req.user.id });
+
+        await User.findOneAndRemove({ _id: req.user.id });
+        
+        res.json({ msg: 'User deleted' });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
 })
 
 module.exports = router;
