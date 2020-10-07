8b31e30985a05dd1078dd0a1bbfb3a20655ed9e7
Mike Wittenauer <mikewittenauer@me.com>
2020-10-06 20:41:37 -0400

    Finish wiring up alert redux/components

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Navbar } from './components/layout/Navbar';
 import { Landing } from './components/layout/Landing';
-import { Register } from './components/auth/Register';
+import Register from './components/auth/Register';
 import { Login } from './components/auth/Login';
+import Alert from './components/layout/Alert';
 import './App.css';
 // Redux
 import { Provider } from 'react-redux';
@@ -16,6 +17,7 @@ const App = () =>
         <Navbar />
         <Route exact path="/" component={Landing} />
         <section className="container">
+          <Alert />
           <Switch>
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
