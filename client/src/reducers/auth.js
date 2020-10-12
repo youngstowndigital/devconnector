78bdac341faabe8fd716a3700f9c0ddf36e757eb
Mike Wittenauer <mikewittenauer@me.com>
2020-10-12 19:52:52 -0400

    Setup get user in reducers

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,6 +1,8 @@
 import { 
     REGISTER_SUCCESS, 
-    REGISTER_FAIL 
+    REGISTER_FAIL ,
+    USER_LOADED,
+    AUTH_ERROR
 } from '../actions/types';
 
 const initialState = {
@@ -14,7 +16,15 @@ export default function(state = initialState, action) {
     const { type, payload } = action;
 
     switch(type) {
+        case USER_LOADED:
+            return {
+                ...state,
+                isAuthenticated: true,
+                loading: false,
+                user: payload
+            }
         case REGISTER_SUCCESS:
+        case AUTH_ERROR:
             localStorage.setItem('token', payload.token);
             return {
                 ...state,
