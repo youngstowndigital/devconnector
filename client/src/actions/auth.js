d5962a170fa14a20c901b011e3f3846a52a313e8
Mike Wittenauer <mikewittenauer@me.com>
2020-10-22 20:18:21 -0400

    Finish login functionality

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -5,13 +5,15 @@ import {
     REGISTER_SUCCESS,
     REGISTER_FAIL,
     USER_LOADED,
-    AUTH_ERROR
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL
 } from './types';
 
 // Load User
 export const loadUser = () => async dispatch => {
-    if (localStorage.token) {
-        setAuthToken(localStorage.token);
+    if (localStorage.getItem('token')) {
+        setAuthToken(localStorage.getItem('token'));
     }
 
     try {
@@ -44,6 +46,8 @@ export const register = ({ name, email, password }) => async dispatch => {
             type: REGISTER_SUCCESS,
             payload: res.data
         });
+
+        dispatch(loadUser());
     } catch (error) {
         const errors = error.response.data.errors;
         if (errors) {
@@ -51,6 +55,37 @@ export const register = ({ name, email, password }) => async dispatch => {
         }
         dispatch({
             type: REGISTER_FAIL
+        });
+    }
+}
+
+export const login = (email, password) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    const body = JSON.stringify({ email, password });
+
+    try {
+        const res = await axios.post('/api/auth', body, config);
+
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data
+        });
+
+        dispatch(loadUser());
+    } catch (err) {
+        const errors = err.response.data.errors;
+
+        if (errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
+        dispatch({
+            type: LOGIN_FAIL
         });
     }
 }
