5ef6a06c0c695a905136e53ed044de11c9126c2e
Mike Wittenauer <mikewittenauer@me.com>
2020-11-01 14:25:29 -0500

    Create spinner gif for loading profile

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,26 +1,27 @@
-import React, { useEffect } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types'
+import Spinner from '../layout/Spinner';
 import { getCurrentProfile } from '../../actions/profile';
 
-const Dashboard = ({ getCurrentProfile, profile }) => {
+const Dashboard = ({ getCurrentProfile, profile: { profile, loading }, auth }) => {
     useEffect(() => {
         getCurrentProfile()
     }, []);
 
-    return (
-        <div>
-            Dashboard
-        </div>
-    )
+    return loading && profile === null ? <Spinner /> : <Fragment>
+        Test
+    </Fragment>;
 }
 
 Dashboard.propTypes = {
     profile: PropTypes.object.isRequired,
-    getCurrentProfile: PropTypes.func.isRequired
+    getCurrentProfile: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
 }
 
 const mapStateToProps = state => ({
+    auth: state.auth,
     profile: state.profile
 })
 
