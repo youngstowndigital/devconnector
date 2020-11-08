5b678982ee1b2e97ce2aee16b46f1464b3badbea
Mike Wittenauer <mikewittenauer@me.com>
2020-11-08 16:07:28 -0500

    Wire up create profile form

--- client/src/components/profile-form/CreateProfile.js
+++ client/src/components/profile-form/CreateProfile.js
@@ -35,6 +35,9 @@ const CreateProfile = props => {
         instagram
     } = formData;
 
+    const onChange = (e) =>
+        setFormData({ ...formData, [e.event.name]: e.target.value });
+
     return (
         <Fragment>
             <h1 className="large text-primary">
@@ -47,7 +50,7 @@ const CreateProfile = props => {
             <small>* = required field</small>
             <form className="form">
                 <div className="form-group">
-                <select name="status">
+                <select name="status" value={status} onChange={onChange}>
                     <option value="0">* Select Professional Status</option>
                     <option value="Developer">Developer</option>
                     <option value="Junior Developer">Junior Developer</option>
@@ -63,25 +66,25 @@ const CreateProfile = props => {
                 >
                 </div>
                 <div className="form-group">
-                <input type="text" placeholder="Company" name="company" />
+                <input type="text" placeholder="Company" name="company" value={company} onChange={onChange} />
                 <small className="form-text"
                     >Could be your own company or one you work for</small
                 >
                 </div>
                 <div className="form-group">
-                <input type="text" placeholder="Website" name="website" />
+                <input type="text" placeholder="Website" name="website" value={website} onChange={onChange} />
                 <small className="form-text"
                     >Could be your own or a company website</small
                 >
                 </div>
                 <div className="form-group">
-                <input type="text" placeholder="Location" name="location" />
+                <input type="text" placeholder="Location" name="location" value={location} onChange={onChange} />
                 <small className="form-text"
                     >City & state suggested (eg. Boston, MA)</small
                 >
                 </div>
                 <div className="form-group">
-                <input type="text" placeholder="* Skills" name="skills" />
+                <input type="text" placeholder="* Skills" name="skills" value={skills} onChange={onChange} />
                 <small className="form-text"
                     >Please use comma separated values (eg.
                     HTML,CSS,JavaScript,PHP)</small
@@ -92,6 +95,7 @@ const CreateProfile = props => {
                     type="text"
                     placeholder="Github Username"
                     name="githubusername"
+                    value={githubusername} onChange={onChange}
                 />
                 <small className="form-text"
                     >If you want your latest repos and a Github link, include your
@@ -99,7 +103,7 @@ const CreateProfile = props => {
                 >
                 </div>
                 <div className="form-group">
-                <textarea placeholder="A short bio of yourself" name="bio"></textarea>
+                <textarea placeholder="A short bio of yourself" name="bio" value={bio} onChange={onChange}></textarea>
                 <small className="form-text">Tell us a little about yourself</small>
                 </div>
 
@@ -112,22 +116,22 @@ const CreateProfile = props => {
                 { displaySocialInputs && <Fragment>
                     <div className="form-group social-input">
                     <i className="fab fa-twitter fa-2x"></i>
-                    <input type="text" placeholder="Twitter URL" name="twitter" />
+                    <input type="text" placeholder="Twitter URL" name="twitter" value={twitter} onChange={onChange} />
                     </div>
 
                     <div className="form-group social-input">
                     <i className="fab fa-facebook fa-2x"></i>
-                    <input type="text" placeholder="Facebook URL" name="facebook" />
+                    <input type="text" placeholder="Facebook URL" name="facebook" value={facebook} onChange={onChange} />
                     </div>
 
                     <div className="form-group social-input">
                     <i className="fab fa-youtube fa-2x"></i>
-                    <input type="text" placeholder="YouTube URL" name="youtube" />
+                    <input type="text" placeholder="YouTube URL" name="youtube" value={youtube} onChange={onChange} />
                     </div>
 
                     <div className="form-group social-input">
                     <i className="fab fa-linkedin fa-2x"></i>
-                    <input type="text" placeholder="Linkedin URL" name="linkedin" />
+                    <input type="text" placeholder="Linkedin URL" name="linkedin" value={linkedin} onChange={onChange} />
                     </div>
 
                     <div className="form-group social-input">
