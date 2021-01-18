4dae989a85fa6e15552c744e7f75d274bad1765b
Mike Wittenauer <mikewittenauer@me.com>
2021-01-18 18:59:38 -0500

    Delete post functionality

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -4,7 +4,8 @@ import setAuthToken from '../utils/setAuthToken';
 import {
     GET_POSTS,
     POST_ERROR,
-    UPDATE_LIKES
+    UPDATE_LIKES,
+    DELETE_POST
 } from './types';
 
 // Get posts
@@ -65,6 +66,30 @@ export const removeLike = (postId) => async dispatch => {
             type: UPDATE_LIKES,
             payload: { postId, likes: res.data }
         });
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+}
+
+// Delete post
+export const deletePost = (postId) => async dispatch => {
+    if (localStorage.getItem('token')) {
+        setAuthToken(localStorage.getItem('token'));
+    }
+
+    try {
+        const res = await axios.delete(`/api/posts/${postId}`);
+        console.log(res);
+
+        dispatch({
+            type: DELETE_POST,
+            payload: postId
+        });
+
+        dispatch(setAlert('Post Removed', 'success'));
     } catch (err) {
         dispatch({
             type: POST_ERROR,
