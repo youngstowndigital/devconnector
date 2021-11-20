3ac6f55b4d218e62ac35b92e6bb1cf0a8dbd1a30
Mike Wittenauer <mikewittenauer@me.com>
2021-11-20 11:15:01 -0500

    Register user upon register post

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const { check, validationResult } = require('express-validator');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
+
+const User = require('../../models/User');
 
 // @route   POST api/users
 // @desc    Register user
@@ -14,8 +18,34 @@ router.post('/',[
     if (!errors.isEmpty())
         return res.status(400).json({ errors: errors.array() });
 
-    console.log(req.body);
-    res.send('User route');
+    const { name, email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+
+        if (user)
+            return res.status(400).json({ errors: [{ msg: 'User already exists'}] });
+
+        const avatar = gravatar.url(email, { s: '200', r: 'pg', d: 'mm' });
+
+        user = new User({
+            name,
+            email,
+            avatar,
+            password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        res.send('User registered');
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error');
+    }
 });
 
 module.exports = router;
