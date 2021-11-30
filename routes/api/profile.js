42de696a3a0ca9c7b068cbf44ea49508b3407430
Mike Wittenauer <mikewittenauer@me.com>
2021-11-29 19:29:10 -0500

    Get profiles and get profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -92,4 +92,38 @@ router.post('/', [
     }
 });
 
+// @route   GET api/profile
+// @desc    Get all profiles
+// @acess   Public
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+
+        res.json(profiles);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error'); 
+    }
+});
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile
+// @access  Public
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar']);
+
+        if (!profile)
+            return res.status(400).json({ msg: 'Profile not found' });
+
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        if (error.kind == 'ObjectId') {
+            return res.status(400).json({ msg: 'Profile not found' });
+        }
+        res.status(500).send('Server error'); 
+    }
+});
+
 module.exports = router;
